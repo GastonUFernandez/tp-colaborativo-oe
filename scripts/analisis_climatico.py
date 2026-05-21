3ce392f7c598c90e055f72ab2834e5e44fc058e9
Gastón Uriel Fernández <gaston.u.fernandez@hotmail.com>
2026-05-21 16:29:24 -0300

    SCRUM-8: Fix: corregir KeyError cambiando columnas a inglés vía API

--- scripts/analisis_climatico.py
+++ scripts/analisis_climatico.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 def analizar_clima(ruta_csv):
     df = pd.read_csv(ruta_csv)
-    media = df['promedio'].mean()
+    media = df['Mean'].mean()
     print(f"--- REPORTE ESCENARIO A ---")
     print(f"Media anual: {media}")
     
-    plt.plot(df['año'], df['promedio'])
+    plt.plot(df['Year'], df['Mean'])
     plt.title('Tendencia Climática')
     plt.savefig('resultados/grafico_tendencia.png')
     print("Gráfico generado en /resultados")
